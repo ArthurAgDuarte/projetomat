a5570dc36762783efeef7158068e85a256c30a0d
ArthurAgDuarte <duartearthur30@gmail.com>
2025-05-06 15:59:23 -0300

    mudando de novo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,17 +8,17 @@ export default function Home(){
        <main className='home'>
         <Card
         titulo={"Conjuntos"}
-        link={"/projetomat/conjuntos"}
+        link={"/conjuntos"}
         imagem={"./public/Venn_A_intersect_B.svg.png"}
         />
         <Card
         titulo={"Relações"}
-        link={"/projetomat/relacoes"}
+        link={"/relacoes"}
         imagem={"public/Relación_binaria_12.svg.png"}
         />
         <Card
         titulo={"Funções"}
-        link={"/projetomat/funcoes"}
+        link={"/funcoes"}
         imagem={"public/Graph_of_a_function.svg.png"}
         />
        </main>
